f81c2829229c781e8aef928f59ce66d9c104e342
PGrace <pavg333777@gmail.com>
2021-10-17 20:12:14 +0300

    git1

---
+++ 1.py
@@ -0,0 +1,8 @@
+#name=(Соня)
+#age= 8
+name=input("Ввведите своё имя: ")
+age=input("Укажите свой возраст: ")
+
+print("Привет, " + name + "!")
+print("Тебе уже " + age + " лет, это так круто!!! =D" )
+print ('Good night my dear Я тебя люблю')
---
+++ 3.py
@@ -0,0 +1,23 @@
+#import pyowm
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
+#owm = pyowm.OWM('93c4ed227d4d86a037d1a7092188d7df')
+
+#place = input ("В каком городе/стране?: ")
+
+#observation = owm.weather_at_place(place)
+#w = observation.get_weather()
+
+#print(w)
+
+owm = OWM('93c4ed227d4d86a037d1a7092188d7df')
+mgr = owm.weather_manager()
+
+place = input ("В каком городе/стране?: ")
+observation = mgr.weather_at_place(place)
+w = observation.weather
+print(w)
+
+
+print(олдыводл)
